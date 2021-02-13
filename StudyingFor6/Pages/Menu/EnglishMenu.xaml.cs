a2124156e0901010cb5e4028f010baa3dbfe8d59
sergy <sergy@LAPTOP-RNKUBBEN>
2021-02-13 12:38:46 +0300

    Добавьте файлы проекта.

---
+++ StudyingFor6/Pages/Menu/EnglishMenu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudyingFor6.Pages.Menu
+{
+    /// <summary>
+    /// Логика взаимодействия для EnglishMenu.xaml
+    /// </summary>
+    public partial class EnglishMenu : Page
+    {
+        public EnglishMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+    }
+}
---
+++ StudyingFor6/Pages/Menu/GamesMenu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudyingFor6.Pages.Menu
+{
+    /// <summary>
+    /// Логика взаимодействия для GamesMenu.xaml
+    /// </summary>
+    public partial class GamesMenu : Page
+    {
+        public GamesMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+    }
+}
